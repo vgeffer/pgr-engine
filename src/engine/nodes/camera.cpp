323b15f28c9a5acd05444412b6e5e1ae8360bb8c
vgeffer <geffevil@fel.cvut.cz>
2025-03-11 15:50:06 +0100

    Camera now recomputes projection matrix on each param change

--- src/engine/nodes/camera.cpp
+++ src/engine/nodes/camera.cpp
@@ -1,4 +1,5 @@
 #include <cmath>
+#include <glm/fwd.hpp>
 #include "camera.hpp"
 #include "scene_node.hpp"
 #include "../runtime.hpp"
@@ -6,19 +7,44 @@
 using namespace glm;
 using namespace nodes;
 
-camera::camera(float fov, float near, float far) 
-    : scene_node(), fov(fov), near(near), far(far) {
-
+static inline mat4x4 _make_projection(float fov, float near, float far) {
     win_props_t& win = engine_runtime::instance()->window()->props();
 
     float tan_fov = tanf(glm::radians(fov / 2));
     float asp_ratio = (float)win.current_mode->w / (float)win.current_mode->h;
 
-    projection =  mat4x4(
+    return mat4x4(
         1.0f / tan_fov, 0, 0, 0,
         0, 1.0f / (tan_fov * asp_ratio) ,  0, 0,
         0, 0, -((far + near) / (far - near)), -1,
         0, 0, -((2.0f * far * near) / (far - near)), 0
     ); 
+}
+
+camera::camera(float fov, float near, float far) 
+    : scene_node(), _fov(fov), _near(near), _far(far), _projection(_make_projection(fov, near, far)) {}
+
+
+mat4x4 camera::view() const {
+
+    /* Recompute view */
+}
+
+float camera::fov(float& fov) {
+
+    _projection = _make_projection(fov, _near, _far);
+    return _fov = fov;
+}
+
+float camera::near(float& near) {
+    
+    _projection = _make_projection(_fov, near, _far);
+    return _near = near;
+}
+
+float camera::far(float& far) {
+
+    _projection = _make_projection(_fov, _near, far);
+    return _far = far;
 }
 
--- src/engine/nodes/camera.hpp
+++ src/engine/nodes/camera.hpp
@@ -10,17 +10,31 @@ namespace nodes {
             camera(float fov, float near, float far);
 
             void look_at(glm::vec3 target);
-
-            glm::mat4x4& get_view();
-            glm::mat4x4& get_projection();
-            glm::vec3& get_forward();
-
             glm::vec3 to_screen(glm::vec3& point);
             
-            float fov, near, far;
+            
+            glm::mat4x4 view() const;
+            inline glm::mat4x4 projection() const { return _projection; }
+            inline glm::vec3 forward() const { return _fwd; }
+
+            inline float fov() const { return _fov; }
+            inline float near() const { return _near; }
+            inline float far() const { return _far; }
+
+            float fov(float& fov);
+            float near(float& near);
+            float far(float& far);
+
+            GLint render_target() const;
+            GLint render_target(GLint& target);
 
         private:
-            glm::vec3 fwd;
-            glm::mat4x4 projection;
+
+
+
+            glm::vec3 _fwd;
+            glm::mat4x4 _projection;
+            float _fov, _near, _far;
+            GLint _render_target;
     };
 }
