3e693a4c2b93e51ecf4463995df889ac2330a0e8
vgeffer <geffevil@fel.cvut.cz>
2025-04-01 13:01:24 +0200

    Dropped tinyxml2 dependency

--- src/lib/tinyxml2/project.mk
+++
@@ -1,9 +0,0 @@
-PROJECT_NAME := tinyxml2
-PROJECT_TYPE := static
-PROJECT_DEPENDENCIES :=
-
-COMPILER := CXX
-EXTRA_BUILD_FLAGS := -fPIC 
-
-include $(BASE_DIR)/config.mk
-include $(BASE_DIR)/$(SOURCE_DIR)/build.mk
