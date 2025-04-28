27d1578fe9ae30eb8b217f4ee9799c6958133e56
vgeffer <geffevil@fel.cvut.cz>
2025-04-28 13:28:36 +0200

    Added OpenGL debug code

--- src/engine/runtime.cpp
+++ src/engine/runtime.cpp
@@ -24,16 +24,61 @@ using namespace physics;
 using namespace rendering;
 using namespace std::chrono;
 
+#ifndef NDEBUG
+
+    void APIENTRY glDebugOutput(GLenum source, GLenum type, unsigned int id, GLenum severity, GLsizei length, const char *message, const void *userParam) {
+        std::cout << "---------------" << std::endl;
+        std::cout << "Debug message (" << id << "): " <<  message << std::endl;
+
+        switch (source)
+        {
+            case GL_DEBUG_SOURCE_API:             std::cout << "Source: API\n"; break;
+            case GL_DEBUG_SOURCE_WINDOW_SYSTEM:   std::cout << "Source: Window System\n"; break;
+            case GL_DEBUG_SOURCE_SHADER_COMPILER: std::cout << "Source: Shader Compiler\n"; break;
+            case GL_DEBUG_SOURCE_THIRD_PARTY:     std::cout << "Source: Third Party\n"; break;
+            case GL_DEBUG_SOURCE_APPLICATION:     std::cout << "Source: Application\n"; break;
+            case GL_DEBUG_SOURCE_OTHER:           std::cout << "Source: Other\n"; break;
+        }
+
+        switch (type)
+        {
+            case GL_DEBUG_TYPE_ERROR:               std::cout << "Type: Error\n"; break;
+            case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated Behaviour\n"; break;
+            case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "Type: Undefined Behaviour\n"; break; 
+            case GL_DEBUG_TYPE_PORTABILITY:         std::cout << "Type: Portability\n"; break;
+            case GL_DEBUG_TYPE_PERFORMANCE:         std::cout << "Type: Performance\n"; break;
+            case GL_DEBUG_TYPE_MARKER:              std::cout << "Type: Marker\n"; break;
+            case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "Type: Push Group\n"; break;
+            case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "Type: Pop Group\n"; break;
+            case GL_DEBUG_TYPE_OTHER:               std::cout << "Type: Other\n"; break;
+        }
+
+        switch (severity)
+        {
+            case GL_DEBUG_SEVERITY_HIGH:         std::cout << "Severity: high\n"; break;
+            case GL_DEBUG_SEVERITY_MEDIUM:       std::cout << "Severity: medium\n"; break;
+            case GL_DEBUG_SEVERITY_LOW:          std::cout << "Severity: low\n"; break;
+            case GL_DEBUG_SEVERITY_NOTIFICATION: std::cout << "Severity: notification\n"; break;
+        }
+        std::cout << std::endl;
+    }
+#endif
+
+
 /* Tie singletons with the app */
 events g_events = events();
-
 
 engine_runtime::engine_runtime(game_window& window) 
     : _root_node(new scene_node("GLOBAL_ROOT", scene_node::node_type::ROOT)), _window(window) {
 
     _instance = this;
 
-
+    #ifndef NDEBUG
+        glEnable(GL_DEBUG_OUTPUT);
+        glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS); 
+        glDebugMessageCallback(glDebugOutput, nullptr);
+        glDebugMessageControl(GL_DONT_CARE, GL_DONT_CARE, GL_DONT_CARE, 0, nullptr, GL_TRUE);
+    #endif
 
     _renderer = make_unique<renderer>();
     _physics = make_unique<physics_engine>();
@@ -80,7 +125,7 @@ void engine_runtime::start() {
         tp_now = system_clock::now();
         float elapsed = duration_cast<milliseconds>(tp_now - tp_prev).count() / 1000.0f;
         tp_prev = tp_now;        
-
+        
         physics_delta += elapsed;
 
         /* Physics */
