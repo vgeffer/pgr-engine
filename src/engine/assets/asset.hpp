51122a5ecc0635022b3a8ca5f7bbf43c5ba558c7
vgeffer <geffevil@fel.cvut.cz>
2025-05-06 15:35:02 +0200

    Added cubemap asset

--- src/engine/assets/asset.hpp
+++ src/engine/assets/asset.hpp
@@ -1,13 +1,16 @@
 ///
 /// @file asset.hpp
 /// @author geffevil
-/// @brief A base class for all assets
 ///
 #pragma once
 
-
 namespace assets {
 
+    /// @brief Base class for all pgr-engine's assets
+    ///
+    /// To qualify as asset, class must publicly derive from this class
+    /// Class must also provide constructor which takes one parameter, @c{const string& path}
+    /// The above conditions must be met to be able to load the asset using @c assets::loader::load
     class asset {
         public:
             /// @brief Default destructor for class - assuring proper construction/destruction 
